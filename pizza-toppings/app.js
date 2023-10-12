36b16bcdc1f0d5c4113e0c1bdeced4040b569f74
diirf <freinva21@gmail.com>
2023-10-12 19:12:56 +0200

    improvement of the pizza topping code making an array of pizza objects

--- pizza-toppings/app.js
+++ pizza-toppings/app.js
@@ -1,154 +1,28 @@
 const pizzaTypes = document.getElementById('pizza-types');
+const toppingsList = document.getElementById('toppings-list');
 
-function changePizzaType(){
-    switch (pizzaTypes.selectedIndex){
-        case 1:
-            document.getElementById('base-pomodoro-item').classList.remove('hide');
-            document.getElementById('mozarella-item').classList.remove('hide');
-            document.getElementById('fontina-item').classList.add('hide')
-            document.getElementById('emmental-item').classList.add('hide');
-            document.getElementById('gorgonzola-item').classList.add('hide')
-            document.getElementById('parmigiano-item').classList.add('hide')
-            document.getElementById('prosciutto-item').classList.add('hide');
-            document.getElementById('peperoncini-item').classList.add('hide');
-            document.getElementById('salame-item').classList.add('hide')
-            document.getElementById('olive-item').classList.add('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.add('hide');
-            document.getElementById('carciofini-item').classList.add('hide');
-            document.getElementById('acciughe-item').classList.add('hide');
-            document.getElementById('pomodoro-item').classList.add('hide')
-            document.getElementById('origano-item').classList.remove('hide')
-            document.getElementById('basilico-item').classList.remove('hide');
-            document.getElementById('pepe-item').classList.add('hide');
-            break;
-
-        case 2:
-            document.getElementById('base-pomodoro-item').classList.add('hide');
-            document.getElementById('mozarella-item').classList.remove('hide');
-            document.getElementById('fontina-item').classList.remove('hide')
-            document.getElementById('emmental-item').classList.add('hide');
-            document.getElementById('gorgonzola-item').classList.remove('hide')
-            document.getElementById('parmigiano-item').classList.remove('hide')
-            document.getElementById('prosciutto-item').classList.add('hide');
-            document.getElementById('peperoncini-item').classList.add('hide');
-            document.getElementById('salame-item').classList.add('hide')
-            document.getElementById('olive-item').classList.add('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.add('hide');
-            document.getElementById('carciofini-item').classList.add('hide');
-            document.getElementById('acciughe-item').classList.add('hide');
-            document.getElementById('pomodoro-item').classList.add('hide')
-            document.getElementById('origano-item').classList.add('hide')
-            document.getElementById('basilico-item').classList.add('hide');
-            document.getElementById('pepe-item').classList.remove('hide');
-            break;
-        
-        case 3:
-            document.getElementById('base-pomodoro-item').classList.remove('hide');
-            document.getElementById('mozarella-item').classList.remove('hide');
-            document.getElementById('fontina-item').classList.add('hide')
-            document.getElementById('emmental-item').classList.remove('hide');
-            document.getElementById('gorgonzola-item').classList.add('hide')
-            document.getElementById('parmigiano-item').classList.add('hide')
-            document.getElementById('prosciutto-item').classList.add('hide');
-            document.getElementById('peperoncini-item').classList.remove('hide');
-            document.getElementById('salame-item').classList.remove('hide')
-            document.getElementById('olive-item').classList.add('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.add('hide');
-            document.getElementById('carciofini-item').classList.add('hide');
-            document.getElementById('acciughe-item').classList.add('hide');
-            document.getElementById('pomodoro-item').classList.add('hide')
-            document.getElementById('origano-item').classList.add('hide')
-            document.getElementById('basilico-item').classList.add('hide');
-            document.getElementById('pepe-item').classList.add('hide');
-            break;
-        
-        case 4:
-            document.getElementById('base-pomodoro-item').classList.remove('hide');
-            document.getElementById('mozarella-item').classList.add('hide');
-            document.getElementById('fontina-item').classList.add('hide')
-            document.getElementById('emmental-item').classList.add('hide');
-            document.getElementById('gorgonzola-item').classList.add('hide')
-            document.getElementById('parmigiano-item').classList.add('hide')
-            document.getElementById('prosciutto-item').classList.remove('hide');
-            document.getElementById('peperoncini-item').classList.add('hide');
-            document.getElementById('salame-item').classList.add('hide')
-            document.getElementById('olive-item').classList.remove('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.remove('hide');
-            document.getElementById('carciofini-item').classList.remove('hide');
-            document.getElementById('acciughe-item').classList.add('hide');
-            document.getElementById('pomodoro-item').classList.add('hide')
-            document.getElementById('origano-item').classList.add('hide')
-            document.getElementById('basilico-item').classList.add('hide');
-            document.getElementById('pepe-item').classList.add('hide');
-            break;
-        
-        case 5:
-            document.getElementById('base-pomodoro-item').classList.remove('hide');
-            document.getElementById('mozarella-item').classList.remove('hide');
-            document.getElementById('fontina-item').classList.add('hide')
-            document.getElementById('emmental-item').classList.add('hide');
-            document.getElementById('gorgonzola-item').classList.add('hide')
-            document.getElementById('parmigiano-item').classList.add('hide')
-            document.getElementById('prosciutto-item').classList.add('hide');
-            document.getElementById('peperoncini-item').classList.add('hide');
-            document.getElementById('salame-item').classList.add('hide')
-            document.getElementById('olive-item').classList.add('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.remove('hide');
-            document.getElementById('carciofini-item').classList.add('hide');
-            document.getElementById('acciughe-item').classList.add('hide');
-            document.getElementById('pomodoro-item').classList.add('hide')
-            document.getElementById('origano-item').classList.remove('hide')
-            document.getElementById('basilico-item').classList.add('hide');
-            document.getElementById('pepe-item').classList.add('hide');
-            break;
-        
-        case 6:
-            document.getElementById('base-pomodoro-item').classList.remove('hide');
-            document.getElementById('mozarella-item').classList.add('hide');
-            document.getElementById('fontina-item').classList.add('hide')
-            document.getElementById('emmental-item').classList.add('hide');
-            document.getElementById('gorgonzola-item').classList.add('hide')
-            document.getElementById('parmigiano-item').classList.add('hide')
-            document.getElementById('prosciutto-item').classList.add('hide');
-            document.getElementById('peperoncini-item').classList.add('hide');
-            document.getElementById('salame-item').classList.add('hide')
-            document.getElementById('olive-item').classList.add('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.add('hide');
-            document.getElementById('carciofini-item').classList.add('hide');
-            document.getElementById('acciughe-item').classList.remove('hide');
-            document.getElementById('pomodoro-item').classList.remove('hide')
-            document.getElementById('origano-item').classList.remove('hide')
-            document.getElementById('basilico-item').classList.add('hide');
-            document.getElementById('pepe-item').classList.add('hide');
-            break;
-        
-        case 7:
-            document.getElementById('base-pomodoro-item').classList.remove('hide');
-            document.getElementById('mozarella-item').classList.remove('hide');
-            document.getElementById('fontina-item').classList.add('hide')
-            document.getElementById('emmental-item').classList.add('hide');
-            document.getElementById('gorgonzola-item').classList.add('hide')
-            document.getElementById('parmigiano-item').classList.add('hide')
-            document.getElementById('prosciutto-item').classList.remove('hide');
-            document.getElementById('peperoncini-item').classList.add('hide');
-            document.getElementById('salame-item').classList.add('hide')
-            document.getElementById('olive-item').classList.add('hide');
-            document.getElementById('cipolla-item').classList.add('hide');
-            document.getElementById('funghi-item').classList.remove('hide');
-            document.getElementById('carciofini-item').classList.remove('hide');
-            document.getElementById('acciughe-item').classList.add('hide');
-            document.getElementById('pomodoro-item').classList.add('hide')
-            document.getElementById('origano-item').classList.add('hide')
-            document.getElementById('basilico-item').classList.add('hide');
-            document.getElementById('pepe-item').classList.add('hide');
-            break;
-    }
+function changePizza(){
+  Array.from(toppingsList.children).forEach(child => child.remove());
+  const selectedpizza = pizzas.find(
+    pizza => pizza.name === pizzaTypes.selectedOptions[0].innerText);
+    
+    if (selectedpizza){
+      selectedpizza.ingredients.forEach((ingredient, index) => {
+        const newTopping = document.createElement('li');
+        newTopping.classList.add('toppings');
+        if (index % 3 === 0){
+          newTopping.classList.add('green');
+        }
+        else if (index % 3 === 1){
+          newTopping.classList.add('white');
+        }
+        else{
+          newTopping.classList.add('red');
+        }
+        newTopping.innerText = ingredient
+        toppingsList.appendChild(newTopping);
+      });
+  }
 }
 
-pizzaTypes.addEventListener('change',changePizzaType);
+pizzaTypes.addEventListener('change',changePizza);
---
+++ pizza-toppings/pizzas.js
@@ -0,0 +1,30 @@
+const pizzas = [
+  {
+      "name": "Margherita",
+      "ingredients": ["Base di pomodoro", "Formaggio: Mozzarella", "Origano", "Basilico"]
+  },
+  {
+      "name": "Quattro Formaggi",
+      "ingredients": ["Formaggio: Mozzarella", "Formaggio: Fontina", "Formaggio: Gorgonzola", "Formaggio: Parmigiano", "Pepe"]
+  },
+  {
+      "name": "Alla Diavola",
+      "ingredients": ["Base di pomodoro", "Formaggio: Mozzarella", "Formaggio: Emmental", "Salame", "Peperoncini"]
+  },
+  {
+      "name": "Quattro Stagioni",
+      "ingredients": ["Base di pomodoro", "Prosciutto cotto", "Olive", "Funghi", "Carciofini"]
+  },
+  {
+      "name": "Funghi",
+      "ingredients": ["Base di pomodoro", "Formaggio: Mozzarella", "Funghi", "Origano"]
+  },
+  {
+      "name": "Napoli",
+      "ingredients": ["Base di pomodoro", "Acciughe", "Pomodoro", "Origano"]
+  },
+  {
+      "name": "Capricciosa",
+      "ingredients": ["Base di pomodoro", "Formaggio: Mozzarella", "Prosciutto cotto", "Funghi", "Carciofini"]
+  }
+]   
